64752eaeff82d41ece4023c9fc3a7419ba1a1c1f
Petr Chmelar <petr.chmelar@matee.cz>
2020-01-27 23:57:02 +0100

    Homebrew and Ruby setup

--- setup.sh
+++ setup.sh
@@ -5,6 +5,20 @@ cp .zshenv ~/.zshenv
 cp .zshrc ~/.zshrc
 cp .zlogin ~/.zlogin
 
+# Setup Homebrew and install frequently used brews
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install carthage
+brew install pngquant
+brew install rbenv
+brew install awscli
+
+# Setup Ruby and install frequently used gems
+rbenv install 2.6.3
+rbenv global 2.6.3
+~/.rbenv/shims/gem install bundler
+~/.rbenv/shims/gem install xcode-install
+~/.rbenv/shims/gem install iStats
+
 # Disable icon bounce in Dock
 defaults write com.apple.dock no-bouncing -bool TRUE
 
